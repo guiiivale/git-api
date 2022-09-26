c59c956e21a24b6bb23530f1080332ccaecd718a
guiiivale <guigato62@gmail.com>
2022-09-26 09:37:35 -0300

    Finished filters on repositories table

--- app/Http/Livewire/RepositoryTable.php
+++ app/Http/Livewire/RepositoryTable.php
@@ -5,6 +5,8 @@ namespace App\Http\Livewire;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use App\Models\Repository;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Rappasoft\LaravelLivewireTables\Views\Filters\SelectFilter;
 
 class RepositoryTable extends DataTableComponent
 {
@@ -36,18 +38,57 @@ class RepositoryTable extends DataTableComponent
             Column::make("Disabled", "disabled")
                 ->sortable()
                 ->searchable(),
-            Column::make("Reference created at", "reference_created_at")
+            Column::make("Created at", "reference_created_at")
                 ->sortable()
                 ->searchable(),
-            Column::make("Reference pushed at", "reference_pushed_at")
+            Column::make("Last push", "reference_pushed_at")
                 ->sortable()
                 ->searchable(),
-            Column::make("Created at", "created_at")
-                ->sortable()
-                ->searchable(),
-            Column::make("Updated at", "updated_at")
-                ->sortable()
-                ->searchable(),
+        ];
+    }
+
+    public function filters(): array
+    {
+        return [
+            SelectFilter::make('Private')
+                ->options([
+                    '' => 'All',
+                    '1' => 'Yes',
+                    '0' => 'No',
+                ])
+                ->filter(function(Builder $builder, string $value) {
+                    if ($value === '1') {
+                        $builder->where('private', true);
+                    } elseif ($value === '0') {
+                        $builder->where('private', false);
+                    }
+                }),
+            SelectFilter::make('Archived')
+                ->options([
+                    '' => 'All',
+                    '1' => 'Yes',
+                    '0' => 'No',
+                ])
+                ->filter(function(Builder $builder, string $value) {
+                    if ($value === '1') {
+                        $builder->where('archived', true);
+                    } elseif ($value === '0') {
+                        $builder->where('archived', false);
+                    }
+                }),
+            SelectFilter::make('Disabled')
+                ->options([
+                    '' => 'All',
+                    '1' => 'Yes',
+                    '0' => 'No',
+                ])
+                ->filter(function(Builder $builder, string $value) {
+                    if ($value === '1') {
+                        $builder->where('disabled', true);
+                    } elseif ($value === '0') {
+                        $builder->where('disabled', false);
+                    }
+                }),
         ];
     }
 }
